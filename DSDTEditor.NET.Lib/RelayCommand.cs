373b5d227f6489e6c2ba66a6c1aa53c8605d69d6
Libor Maresch <github@maresch.family>
2018-11-16 15:53:24 +0100

    Adding RelayCommand class

---
+++ DSDTEditor.NET.Lib/RelayCommand.cs
@@ -0,0 +1,149 @@
+﻿namespace DSDTEditor.NET.Lib
+{
+    using System;
+    using System.Diagnostics;
+    using System.Text;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// A command whose sole purpose is to
+    /// relay its functionality to other
+    /// object by invoking delegates. The
+    /// default return value for the CanExecute
+    /// method is 'true'.
+    /// </summary>
+    internal class RelayCommand<T> : ICommand
+    {
+        #region Fields
+        readonly Action<T> _execute = null;
+        readonly Predicate<T> _canExecute = null;
+        #endregion
+
+        #region Constructors
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+        #endregion Constructors
+
+        #region ICommand Members
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this._canExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if (this._canExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute((T)parameter);
+        }
+        #endregion ICommand Members
+    }
+
+    /// <summary>
+    /// A command whose sole purpose is to
+    /// relay its functionality to other
+    /// object by invoking delegates. The
+    /// default return value for the CanExecute
+    /// method is 'true'.
+    /// </summary>
+    internal class RelayCommand : ICommand
+    {
+        #region Fields
+        readonly Action _execute;
+        readonly Func<bool> _canExecute;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Creates a new command that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+        #endregion
+
+        #region ICommand Members
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null ? true : this._canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this._canExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if (this._canExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute();
+        }
+        #endregion ICommand Members
+    }
+}
--- DSDTEditor.NET.Lib/ViewModel.cs
+++ DSDTEditor.NET.Lib/ViewModel.cs
@@ -5,7 +5,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using DSDTEditor.NET.Shared;
+using Microsoft.Win32;
 
 namespace DSDTEditor.NET.Lib
 {
@@ -15,56 +17,92 @@ namespace DSDTEditor.NET.Lib
         private ObservableCollection<IACPIObject> tables;
         private List<string> fileContent;
         private int currentPosition;
-		public ViewModel()
+        private RelayCommand openFileCommand;
+
+        public ViewModel()
         {
             this.tables = new ObservableCollection<IACPIObject>();
-            this.fileName = @"C:\Users\d053898\Downloads\iasl-win-20181031\amltables.dsl";
-            this.currentPosition = 0;
-            this.loadFile();
-            this.createObjects();
         }
 
-		public string FileName
+        public string FileName
         {
             get => this.fileName;
-            set => this.SetField(ref this.fileName, value);
+            private set => this.evaluateSelectedFileAndOpenIt(value);
         }
 
-		public ObservableCollection<IACPIObject> Tables
+        public ObservableCollection<IACPIObject> Tables
         {
             get => this.tables;
             set => this.SetField(ref this.tables, value);
         }
 
-		private void loadFile()
+        public ICommand OpenFileCommand
         {
-            this.fileContent = new List<string>(File.ReadAllLines(this.FileName, Encoding.UTF8));
+            get
+            {
+                if (this.openFileCommand == null)
+                    this.openFileCommand = new RelayCommand(() => this.openDslFile());
+                return this.openFileCommand;
+            }
         }
 
-		private void createObjects()
+        private void openDslFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.DefaultExt = ".dsl";
+            openFileDialog.Filter = "Decompiled Source Language (.dsl)|*.dsl";
+            openFileDialog.Multiselect = false;
+            openFileDialog.Title = "Open Decompiled Source Language File";
+            Nullable<bool> result = openFileDialog.ShowDialog();
+            if (result == true)
+                this.FileName = openFileDialog.FileName;
+        }
+
+        private void evaluateSelectedFileAndOpenIt(string fileName)
+        {
+            if (this.SetField(ref this.fileName, fileName, "FileName"))
+            {
+                if (Path.GetExtension(this.FileName).ToLowerInvariant().Equals(".dsl"))
+                    this.loadFile();
+            }
+        }
+
+        private void loadFile()
+        {
+            if (this.fileContent == null)
+                this.fileContent = new List<string>();
+            else
+                this.fileContent.Clear();
+            this.currentPosition = 0;
+            this.fileContent.AddRange(File.ReadAllLines(this.FileName, Encoding.UTF8));
+            this.Tables.Clear();
+            this.createObjects();
+        }
+
+        private void createObjects()
         {
             List<string> comments = new List<string>();
             bool comment = false;
-			while(currentPosition < this.fileContent.Count)
+            while(currentPosition < this.fileContent.Count)
             {
                 if (this.fileContent[currentPosition].Trim().StartsWith("/*"))
                 {
                     comments.Add(this.fileContent[currentPosition]);
                     comment = true;
                 }
-				else if (this.fileContent[currentPosition].Trim().StartsWith("*/"))
+                else if (this.fileContent[currentPosition].Trim().StartsWith("*/"))
                 {
                     comments.Add(this.fileContent[currentPosition]);
                     comment = false;
                 }
-				else if (this.fileContent[currentPosition].Trim().StartsWith("DefinitionBlock"))
+                else if (this.fileContent[currentPosition].Trim().StartsWith("DefinitionBlock"))
                 {
                     Table table = new Table();
                     if (table.CreateTable(ref currentPosition, this.fileContent, comments))
                         this.Tables.Add(table);
                 }
 
-				if (comment)
+                if (comment)
                     comments.Add(this.fileContent[currentPosition]);
 
                 currentPosition++;
